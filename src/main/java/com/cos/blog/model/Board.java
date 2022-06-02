0c1f16b1c89810b36fee3bc65d0f88437ef8c57a
bosungpark <qkrqhtjd0806@naver.com>
2022-06-02 22:20:29 +0900

    update: edit mappedBy to board model

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -31,9 +32,15 @@ public class Board {
     @ColumnDefault("0")
     private int count; //조회수
 
-    @ManyToOne() //Many=Board, One=User
+//    fetch = FetchType.EAGER -> 실행과 동시에 모든 데이터를 가져온다.
+//    fetch = FetchType.LAZY -> 필요할 때, 필요한 만큼의 정보만을 가져온다.
+    @ManyToOne(fetch = FetchType.EAGER) //Many=Board, One=User
     @JoinColumn(name = "userId")
     private User user; //작성자. DB는 객체를 저장할 수 없다. 하자만 자바 orm 에서는 객체를 사용할 수 있다.
+
+    //mappedBy가 적히면 연관관계의 주인이 아니다. 즉 외래키가 아니라는 의미. DB에 컬럼을 만들지 않는다.
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+    private List<Reply> replies;
 
     @CreationTimestamp
     private Timestamp createDate;
