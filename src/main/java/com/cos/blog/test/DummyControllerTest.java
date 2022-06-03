ef09415f7d495a572db620af3a19cd2b3eaa28df
bosungpark <qkrqhtjd0806@naver.com>
2022-06-03 21:22:17 +0900

    update: add paging

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,11 +4,16 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -18,6 +23,21 @@ public class DummyControllerTest {
 //    DummyControllerTest가 메모리에 로드될 때, userRepository 역시 로딩시켜준다. -> 의존성 주입(DI)
     @Autowired
     private UserRepository userRepository;
+
+    // http://localhost:8000/blog/dummy/user
+    @GetMapping("/dummy/users")
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+//    한 페이지 당, 2건의 데이터 리턴
+//    http://localhost:8000/blog/dummy/user?page=페이지 입력
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<User> pagingUsers= userRepository.findAll(pageable);
+        List<User> users= pagingUsers.getContent();
+        return users;
+    }
 
     // {id} 주소로 파라미터를 전달 받을 수 있다.
     //@PathVariable int id
