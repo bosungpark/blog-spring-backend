b4287456b5e703802e795e1e513b4c223bcbecb0
bosungpark <qkrqhtjd0806@naver.com>
2022-06-22 17:39:48 +0900

    update: set hash

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //빈등록: 스프링컨테이너에서 객체를 관리할 수 있게 하는 것
 @Configuration
@@ -14,11 +16,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //IoC
+    @Bean
+    public BCryptPasswordEncoder encodePWD(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/auth/**")
+                .antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,13 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){
         // 실제 DB에 insert를 하고, return.
-        user.setRole(RoleType.USER);
+
         int result= userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), result);
     }
